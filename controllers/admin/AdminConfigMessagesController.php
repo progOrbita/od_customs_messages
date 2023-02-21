6fc9d35844047299e4b5c0e4f45210057eeeb561
progOrbita <web.orbitadigital@gmail.com>
2023-02-21 11:27:07 +0100

    Update AdminConfigMessagesController.php
    add contructor

--- controllers/admin/AdminConfigMessagesController.php
+++ controllers/admin/AdminConfigMessagesController.php
@@ -4,4 +4,11 @@ class AdminConfigMessagesController extends ModuleAdminController
 {
     private $fields_values = [];
 
+    public function __construct()
+    {
+        $this->bootstrap = true;
+        parent::__construct();
+        $this->buildFieldsValues();
+    }
+
 }
