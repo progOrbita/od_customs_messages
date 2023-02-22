5831ca04f994a72cbf269f120925c9dccb96e29f
progOrbita <web.orbitadigital@gmail.com>
2023-02-22 11:14:03 +0100

    Update od_customs_messages.js
    add function addCustomMessage to add message non displayed

--- views/js/od_customs_messages.js
+++ views/js/od_customs_messages.js
@@ -1,1 +1,12 @@
 
+
+/**
+ * function to add the custom message
+ */
+function addCustomMessage() {
+    if ($('#checkout-delivery-step').hasClass('-reachable')) {
+        $('div').remove('.od_custom_message');
+        $('#checkout-delivery-step h1').before("<div class='od_custom_message'>" + window.msg + "</div>");
+        $('.od_custom_message').css("display", "none");
+    }
+}
