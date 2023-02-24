06b84a5c86e47a2552cb111155381bc1623fe12c
progOrbita <web.orbitadigital@gmail.com>
2023-02-24 11:29:52 +0100

    Update od_customs_messages.php
    delete old hook function

--- od_customs_messages.php
+++ od_customs_messages.php
@@ -46,28 +46,11 @@ class Od_customs_messages extends Module
         Tools::redirectAdmin($this->context->link->getAdminLink('AdminConfigMessages'));
     }
 
-    public function hookActionFrontControllerSetMedia()
     {
-        if ($this->context->controller->php_self != "order" || $this->context->cart->id_lang < 0 || !$this->context->cart->id_address_delivery) {
-            return;
-        }
-
         $addr = new Address($this->context->cart->id_address_delivery, $this->context->cart->id_lang);
-        if ($addr->city == "Canarias" || $addr->city == "Melilla" || $addr->city == "Ceuta"){
-            $this->context->controller->registerJavascript(
-                'od_send_email-javascript',
-                $this->_path . 'views/js/od_customs_messages.js',
-                [
-                    'server' => 'remote',
-                    'position' => 'bottom',
-                    'priority' => 1000,
-                ]
-            );
 
-            Media::addJsDef([
                 'msg' => Configuration::get('_OD_SEND_CUSTOMS_MESSAGES_', $this->context->cart->id_lang)
             ]);
         }
-        
     }
 }
